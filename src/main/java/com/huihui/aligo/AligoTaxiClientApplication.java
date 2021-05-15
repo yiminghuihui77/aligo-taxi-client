8028276e22b2c93542d5cd8677e380fa8e61f84f
minghui.y <someonemh@yeah.net>
2021-05-15 23:10:27 +0800

    首次提交

---
+++ src/main/java/com/huihui/aligo/AligoTaxiClientApplication.java
@@ -0,0 +1,16 @@
+package com.huihui.aligo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AligoTaxiClientApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( AligoTaxiClientApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/huihui/aligo/aligotaxiclient/AligoTaxiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huihui.aligo.aligotaxiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AligoTaxiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
